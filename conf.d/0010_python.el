c99b7691dfb2fdd832c862f6af33a9737f8b2902
SK Kim <seungkikim@crucialtec.com>
2014-11-04 09:49:57 +0900

    python mode changed.(unnecessary function has been removed)

--- conf.d/0010_python.el
+++ conf.d/0010_python.el
@@ -1,6 +1,4 @@
 ;; Python mode Setting
 
-(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
-
 ; jedi needs virtualenv installed.(pip install virtualenv)
 (add-hook 'python-mode-hook 'jedi:setup)
