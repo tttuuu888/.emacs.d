74ed108f03042f2de882c61215f69a0971a56ec4
Seungki Kim <tttuuu888@gmail.com>
2014-11-04 09:45:28 +0900

    ibuffer , magit setting has little bit changed

--- conf.d/0006_magit.el
+++ conf.d/0006_magit.el
@@ -2,11 +2,6 @@
 
 (define-key global-map [(f12)] 'magit-status)
 
-(when win32p
-  (progn
-    (add-to-list 'load-path "~/.emacs.d/util/win_magit-1.2.0")
-    (require 'magit)))
-
 (when (not window-system)
   (custom-set-faces
    '(magit-diff-add ((t (:foreground "magenta"))))))
