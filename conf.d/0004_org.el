c48c4712fd9dfbc50e95559353f6bb2440240c85
SeungKi Kim <tttuuu888@gmail.com>
2014-12-21 20:09:01 +0900

    org mode key setting modified

--- conf.d/0004_org.el
+++ conf.d/0004_org.el
@@ -2,8 +2,12 @@
 
 (autoload 'org-mode "org")
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cb" 'org-iswitchb)
-(define-key global-map "\C-cr" 'org-remember)
+
+(defun my-org-mode-hook ()
+  (define-key org-mode-map "\C-cl" 'org-store-link)
+  (define-key org-mode-map "\C-ca" 'org-agenda)
+  (define-key org-mode-map "\C-cb" 'org-iswitchb)
+  (define-key org-mode-map "\C-cr" 'org-remember))
+(add-hook 'org-mode-hook 'my-org-mode-hook)
+
 (setq org-log-done t)
