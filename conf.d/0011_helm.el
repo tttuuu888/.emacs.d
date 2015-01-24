b462badcb0edd385f1086c3d1f82feac192287a0
Seungki Kim <tttuuu888@gmail.com>
2015-01-24 19:00:51 +0900

    helm git, helm grep key combine changed.

--- conf.d/0011_helm.el
+++ conf.d/0011_helm.el
@@ -4,7 +4,7 @@
 (when win32p (require 'anything-git-grep))
 
 (define-key global-map (kbd "C-c i") 'helm-imenu)
-(define-key global-map (kbd "C-c f") 'helm-git-files)
+(define-key global-map (kbd "C-c o") 'helm-git-files) ;Open file
 (if unixp
-     (define-key global-map (kbd "C-c g") 'helm-git-grep-at-point)
-     (define-key global-map (kbd "C-c g") 'anything-git-grep-from-here))
+     (define-key global-map (kbd "C-c p") 'helm-git-grep-at-point) ;greP
+     (define-key global-map (kbd "C-c p") 'anything-git-grep-from-here))
