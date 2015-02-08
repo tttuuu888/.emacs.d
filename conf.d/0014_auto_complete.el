d1c537a2f8712e4c987580356d22e29eb4645e73
SeungKi Kim <tttuuu888@gmail.com>
2015-02-08 22:47:16 +0900

    rollback to simple yasnippet setting

--- conf.d/0014_auto_complete.el
+++
@@ -1,8 +0,0 @@
-;; Auto-complete + Yasnippet setting
-
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
-(ac-config-default)
-
-(require 'yasnippet)
-(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
---
+++ conf.d/0014_yasnippet.el
@@ -0,0 +1,7 @@
+;; Yasnippet setting
+
+(require 'yasnippet)
+
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
