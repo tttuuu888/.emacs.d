b364d1b85e215e047d8803cd2820b8971423ce57
SK Kim <seungkikim@crucialtec.com>
2015-03-04 13:56:41 +0900

    C remove warning function added

--- conf.d/0100_myfunc.el
+++ conf.d/0100_myfunc.el
@@ -57,3 +57,51 @@
   (find-file (make-temp-file 
               (let ((now (decode-time)))
                 (concat (number-to-string (car now)) (number-to-string (car (cdr now))) "_temp")))))
+
+
+
+
+; adding temp C code to remove warnings.
+; the function should be in the shape of below:
+; int temp(int a, int b)
+; {
+; }
+; and it turns to :
+; int temp(int a, intb)
+; {
+;     (void)a;
+;     (void)b;
+;     return (int)0;
+; }
+(defun remove-function-warning (p1 p2)
+  "To remove compiler warning, add the code using every parameters and return proper type"
+  (interactive "r")
+  (let ((x (count-matches "{" p1 p2 )))
+    (goto-char p1)
+    (dotimes (i x)
+      (let ((start (search-forward "("))
+            (ret (progn (beginning-of-line) (thing-at-point 'word)))
+            (end (search-forward ")")))
+        (copy-region-as-kill start (- end 1))
+
+        (progn
+          (search-forward "{") 
+          (insert "\n")
+          (yank))
+
+        (let ((start (search-backward "{"))
+              (end (search-forward "}")))
+          (replace-regexp ",$" " " nil start end)
+          (replace-regexp "," "\n" nil start end)
+          (next-line))
+
+        (let ((start (progn (search-backward "{") (next-line) (beginning-of-line) (point)))
+              (end (progn (search-forward "}") (previous-line) (end-of-line) (point))))
+          (replace-regexp "^.*?\\([0-z]+$\\)" "(void)\\1;" nil start end))
+        
+        (insert (if (equalp ret "void")
+                    (format "\n\treturn;")
+                    (format "\n\treturn (%s)0;" ret)))
+        ))
+    (let ((lastp (point)))
+      (indent-region p1 lastp))))
