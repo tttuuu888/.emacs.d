bbf5651bccc08cb52be3b24930e4ba6cc2177660
SK Kim <seungkikim@crucialtec.com>
2015-04-22 19:31:32 +0900

    config for windows modifed.

--- conf.d/0009_helm.el
+++ conf.d/0009_helm.el
@@ -1,10 +1,5 @@
 ;; Helm Setting
 
-;; anything-git-grep replaces helm-git-grep in Windows (helm-git-grep often stops in Windows)
-(when win32p (require 'anything-git-grep))
-
 (define-key global-map (kbd "C-c i") 'helm-imenu)
-(define-key global-map (kbd "C-c o") 'helm-git-files) ;Open file
-(if unixp
-     (define-key global-map (kbd "C-c p") 'helm-git-grep-at-point) ;greP
-     (define-key global-map (kbd "C-c p") 'anything-git-grep-from-here))
+(define-key global-map (kbd "C-c o") 'helm-git-files)   ;Open file
+(define-key global-map (kbd "C-c p") 'helm-git-grep-at-point) ;greP
