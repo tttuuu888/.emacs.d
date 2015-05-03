26fe6cabd6347c1ef17142801759848dd3bc58e6
SeungKi Kim <tttuuu888@gmail.com>
2015-05-03 23:33:52 +0900

    helm eshell history shortcut added

--- conf.d/0009_helm.el
+++ conf.d/0009_helm.el
@@ -3,3 +3,7 @@
 (define-key global-map (kbd "C-c i") 'helm-imenu)
 (define-key global-map (kbd "C-c o") 'helm-git-files)   ;Open file
 (define-key global-map (kbd "C-c p") 'helm-git-grep-at-point) ;greP
+
+(add-hook 'eshell-mode-hook
+          (lambda ()
+              (define-key eshell-mode-map (kbd "C-c C-l")  'helm-eshell-history)))
