de5c90c9d03f76c34a43ac85cbf57bd220c9f17a
Seungki Kim <tttuuu888@gmail.com>
2015-05-21 02:05:05 +0900

    split window setting , ac for cider repl modified

--- conf.d/0102_clojure.el
+++ conf.d/0102_clojure.el
@@ -2,8 +2,14 @@
 
 (add-hook 'clojure-mode-hook
  (lambda ()
-  (define-key clojure-mode-map (kbd "TAB") 'cider-repl-indent-and-complete-symbol)
-  (define-key clojure-mode-map (kbd "\r") 'newline-and-indent)))
+   (define-key clojure-mode-map (kbd "TAB") 'cider-repl-indent-and-complete-symbol)
+   (define-key clojure-mode-map (kbd "\r") 'newline-and-indent)))
+
+
+(add-hook 'cider-repl-mode-hook
+          (lambda ()
+            (auto-complete-mode t)
+            (ac-cider-setup)))
 
 ; Paredit
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
