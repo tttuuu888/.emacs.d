12842e42759829a3a69bd45b77e014f3f8e2d24e
SeungKi Kim <tttuuu888@gmail.com>
2015-05-25 19:07:14 +0900

    require python mode order changed

--- conf.d/0101_python.el
+++ conf.d/0101_python.el
@@ -1,9 +1,11 @@
 ;; Python mode Setting
 
-(require 'python-mode)
-
-(setq jedi:complete-on-dot t)
 (setq py-install-directory (concat "~/.emacs.d/elpa/" (car (directory-files "~/.emacs.d/elpa/" nil "python-mode*"))))
+(setq jedi:complete-on-dot t)
+
+(require 'python-mode)
 
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
+
+
