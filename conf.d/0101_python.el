eb2fd69f65f18bc9dcc288580e81e2f0c270339a
Seungki Kim <tttuuu888@gmail.com>
2015-05-25 16:53:56 +0900

    py-install-directory should be set correctly for jedi:show-doc

--- conf.d/0101_python.el
+++ conf.d/0101_python.el
@@ -3,6 +3,7 @@
 (require 'python-mode)
 
 (setq jedi:complete-on-dot t)
+(setq py-install-directory (concat "~/.emacs.d/elpa/" (car (directory-files "~/.emacs.d/elpa/" nil "python-mode*"))))
 
 (add-hook 'python-mode-hook 'jedi:setup)
 (add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
