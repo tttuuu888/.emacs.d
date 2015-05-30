64dd6b23244e603ad1a2ef9429a2536c4107a195
Seungki Kim <tttuuu888@gmail.com>
2015-05-30 13:29:37 +0900

    incomplete old yasnipet setting file is removed.

--- conf.d/nouse/0014_yasnippet.el
+++
@@ -1,7 +0,0 @@
-;; Yasnippet setting
-
-(require 'yasnippet)
-
-(define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
