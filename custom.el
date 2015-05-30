7adb76984b6338f60cd15523082d34fd0ff5f493
Seungki Kim <tttuuu888@gmail.com>
2015-05-30 13:12:01 +0900

    ac setting modified

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-quick-help-delay 0.5)
  '(backup-directory-alist (quote ((".*" . "/tmp/"))))
  '(c-basic-offset 4)
  '(c-default-style
