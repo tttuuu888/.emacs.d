b6145c3e69c8dca25ad595ffda3d4325f541c560
SeungKi Kim <tttuuu888@gmail.com>
2015-06-11 01:34:26 +0900

    key-chord modified

--- conf.d/0013_key-chord.el
+++ conf.d/0013_key-chord.el
@@ -3,13 +3,7 @@
 (require 'key-chord)
 (key-chord-mode 1)
 
-(key-chord-define-global " g" 'goto-line)
-(key-chord-define-global " x" 'eshell)
-
-(key-chord-define-global " i" 'previous-multiframe-window)
-(key-chord-define-global " o" 'next-multiframe-window)
-
-(key-chord-define-global " l" 'ibuffer)
-(key-chord-define-global " b" 'ido-switch-buffer)
-
-(key-chord-define-global " e" 'er/expand-region)
+(key-chord-define-global "xc" 'eshell)
+(key-chord-define-global "op" 'other-window)
+(key-chord-define-global "jk" 'ido-switch-buffer)
+(key-chord-define-global "-=" 'er/expand-region)
