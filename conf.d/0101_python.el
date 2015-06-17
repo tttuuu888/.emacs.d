8e7d7c9d01bda0ccad10c53aba96d47229c76ed8
Seungki Kim <tttuuu888@gmail.com>
2015-06-17 17:20:20 +0900

    currently use python instead of ipython

--- conf.d/0101_python.el
+++ conf.d/0101_python.el
@@ -7,13 +7,9 @@
     :mode ("\\.py\\'" . python-mode)
     :interpreter ("python" . python-mode)
     :config
-    (setq py-shell-name "ipython")
+    (setq py-shell-name "python")
     (setq py-split-windows-on-execute-function (quote split-window-horizontally))
     (setq py-install-directory
           (concat "~/.emacs.d/elpa/" (car (directory-files "~/.emacs.d/elpa/" nil "python-mode*"))))
     (setq jedi:complete-on-dot t)
-    (use-package python-mode
-        :ensure t)
-    )
-
-
+    (use-package python-mode))
