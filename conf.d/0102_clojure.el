0b5fac6d37e1e96fd7cddf111aca67aa03015cc2
SeungKi Kim <tttuuu888@gmail.com>
2015-07-11 06:11:38 +0900

    clj-refactor added

--- conf.d/0102_clojure.el
+++ conf.d/0102_clojure.el
@@ -1,9 +1,9 @@
 ;; Clojure Setting
 
 (add-hook 'clojure-mode-hook
- (lambda ()
-   (define-key clojure-mode-map (kbd "TAB") 'cider-repl-indent-and-complete-symbol)
-   (define-key clojure-mode-map (kbd "\r") 'newline-and-indent)))
+          (lambda ()
+            (define-key clojure-mode-map (kbd "TAB") 'cider-repl-indent-and-complete-symbol)
+            (define-key clojure-mode-map (kbd "\r") 'newline-and-indent)))
 
 
 (add-hook 'cider-repl-mode-hook
@@ -13,3 +13,12 @@
 
 ; Paredit
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+(use-package clj-refactor
+    :ensure t
+    :mode "clojure-mode"
+    :init
+    (defun my-clojure-mode-hook ()
+      (clj-refactor-mode 1)
+      (cljr-add-keybindings-with-prefix "C-c C-m"))
+    (add-hook 'clojure-mode-hook 'my-clojure-mode-hook))
