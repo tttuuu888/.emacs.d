d5ee8b2188f6c0a5e27abc330e4d53e74266ec0d
Seungki Kim <tttuuu888@gmail.com>
2015-07-22 13:28:44 +0900

    on going

--- conf.d/0103_scheme.el
+++
@@ -1,7 +0,0 @@
-;; Scheme mode Setting
-
-(use-package chicken-scheme
-    :commands setup-chicken-scheme
-    :init
-    (add-hook 'scheme-mode-hook 'setup-chicken-scheme)
-    (add-hook 'scheme-mode-hook 'enable-paredit-mode))
