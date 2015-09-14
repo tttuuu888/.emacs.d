470c36f87b4952e1483cf35bd905c23ec629c53f
Seungki Kim <tttuuu888@gmail.com>
2015-09-14 09:32:34 +0900

    speed up 2nd step

--- init.el
+++ init.el
@@ -1,29 +1,32 @@
 ;; SK Emacs Setting
 
-(setq gc-cons-threshold 100000000)
+(let ((file-name-handler-alist nil)
+      (default-gc-value gc-cons-threshold))
+  (setq gc-cons-threshold (* 100 1024  1024))
 
-(package-initialize)
-(setq package-archives
-      '(("ELPA" . "http://tromey.com/elpa/")
-        ("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (package-initialize)
+  (setq package-archives
+        '(("ELPA" . "http://tromey.com/elpa/")
+          ("gnu" . "http://elpa.gnu.org/packages/")
+          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
-;; use-package
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
+  ;; use-package
+  (when (not (package-installed-p 'use-package))
+    (package-refresh-contents)
+    (package-install 'use-package))
 
-(add-to-list 'load-path "~/.emacs.d/conf.d/")
-(add-to-list 'load-path "~/.emacs.d/conf.d/sk-utils/")
+  (add-to-list 'load-path "~/.emacs.d/conf.d/")
+  (add-to-list 'load-path "~/.emacs.d/conf.d/sk-utils/")
 
-;; custom file
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+  ;; custom file
+  (setq custom-file "~/.emacs.d/custom.el")
+  (load custom-file)
 
-(require 'use-package)
-(use-package conf-init)
+  (require 'use-package)
+  (use-package conf-init)
 
-(recentf-open-files)
+  (recentf-open-files)
 
-(setq gc-cons-threshold 800000)
+  (setq gc-cons-threshold default-gc-value)
+  )
