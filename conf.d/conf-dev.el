ec7e915f33664ebd4c51e8daadb0ab57d4db0309
Seungki Kim <tttuuu888@gmail.com>
2015-09-17 16:00:13 +0900

    sk-c-dev commands option added

--- conf.d/conf-dev.el
+++ conf.d/conf-dev.el
@@ -11,6 +11,7 @@
               (lambda () (local-set-key (kbd "M-o") 'sk-c-header-switch))))
 
 (use-package sk-dev-utils
+    :commands (create-ch-file create-c-file create-h-file clang-complete-make)
     :bind   (("<f9>"    . sk-make)
              ("C-<f9>"  . sk-rebuild)))
 
