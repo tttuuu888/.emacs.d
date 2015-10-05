e7d5c251c9d3a78c9e18895cc2e484c72ba5c373
Seungki Kim <tttuuu888@gmail.com>
2015-10-05 19:48:26 +0900

    init modification restored.

--- init.el
+++ init.el
@@ -1,32 +1,25 @@
 ;; SK Emacs Setting
 
-(let ((file-name-handler-alist nil)
-      (default-gc-value gc-cons-threshold))
-  (setq gc-cons-threshold (* 100 1024  1024))
-
-  (package-initialize)
-  (setq package-archives
-        '(("ELPA" . "http://tromey.com/elpa/")
-          ("gnu" . "http://elpa.gnu.org/packages/")
-          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
+(setq package-archives
+      '(("ELPA" . "http://tromey.com/elpa/")
+        ("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 
-  ;; use-package
-  (when (not (package-installed-p 'use-package))
-    (package-refresh-contents)
-    (package-install 'use-package))
+;; use-package
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-  (add-to-list 'load-path "~/.emacs.d/conf.d/")
-  (add-to-list 'load-path "~/.emacs.d/conf.d/sk-utils/")
+(add-to-list 'load-path "~/.emacs.d/conf.d/")
+(add-to-list 'load-path "~/.emacs.d/conf.d/sk-utils/")
 
-  ;; custom file
-  (setq custom-file "~/.emacs.d/custom.el")
-  (load custom-file)
+;; custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
-  (require 'use-package)
-  (use-package conf-init)
+(require 'use-package)
+(use-package conf-init)
 
-  (recentf-open-files)
-
-  (setq gc-cons-threshold default-gc-value)
-  )
+(recentf-open-files)
