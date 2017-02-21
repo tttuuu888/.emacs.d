06ee73cc0d5ee848f7db97b94fa51d48327d4594
Seungki Kim <tttuuu888@gmail.com>
2017-02-21 17:31:08 +0900

    when org latex pdf export, seperate each paragraph

--- custom.el
+++ custom.el
@@ -36,7 +36,7 @@
      ("http" . "\\.\\(jpeg\\|jpg\\|png\\|gif\\|svg\\|bmp\\)\\'")
      ("https" . "\\.\\(jpeg\\|jpg\\|png\\|gif\\|svg\\|bmp\\)\\'"))))
  '(org-html-validation-link "")
- '(org-latex-packages-alist (quote (("usepackage" "kotex" nil))))
+ '(org-latex-packages-alist (quote (("" "parskip" nil) ("" "kotex" nil))))
  '(org-log-done t)
  '(org-startup-indented t)
  '(password-cache-expiry 900)
