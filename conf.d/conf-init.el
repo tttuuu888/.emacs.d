7b9d1a3faeb9f40350421df7f0b2088248664368
Seungki Kim <tttuuu888@gmail.com>
2017-11-05 13:32:52 +0900

    keep package-selected-packages value without saving in custom.el

--- conf.d/conf-init.el
+++ conf.d/conf-init.el
@@ -21,15 +21,16 @@
 ;;  M-up, M-down, M-left, and M-right keys.
 (windmove-default-keybindings 'meta)
 
-;; To be removed after emacs updated.
-(defun package--save-selected-packages (&rest opt) nil)
+;; To prevent custom.el file changed.
+(defun package--save-selected-packages (&optional value)
+  (when value (setq package-selected-packages value)))
 
 
 (require 'conf-general)
 (require 'conf-dev)
 
 
-;; To be removed after emacs updated.
+;; To prevent abnormal behavior of package-list-packages.
 (setq package--initialized nil)
 
 
