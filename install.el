96c8a6e0f9f345ac225a18716d5b3d6491615459
Seungki Kim <tttuuu888@gmail.com>
2019-01-10 19:20:24 +0900

    install.el update

--- install.el
+++ install.el
@@ -18,21 +18,23 @@
 ;; Start initializing with below command.
 ;; $ emacs -l ~/.emacs.d/install.el -batch -init
 
+(require 'cl)
+
 (defvar number-of-process 8)
 
 (setq package-archives
       '(("gnu"   . "http://elpa.gnu.org/packages/")
         ("org"   . "https://orgmode.org/elpa/")
         ("melpa" . "http://melpa.org/packages/")))
+
 (package-initialize)
 
-
 (defun get-package-list ()
-  (let ((package-list (list "use-package")))
+  (let ((package-list (list 'use-package)))
     (defmacro use-package (package &rest args)
       (unless (or (memq :disabled args)
                   (memq :ensure args))
-        `(add-to-list 'package-list (symbol-name ',package))))
+        `(add-to-list 'package-list ',package)))
 
     (provide 'use-package)
     (defun package-install (&rest _) nil)
@@ -42,10 +44,48 @@
 
     package-list))
 
+(defun remove-duplicate-packages-in-depth (packages &optional depth)
+  (let* ((result packages)
+         (base-depth (if depth depth 0))
+         (base-packages (nth-value base-depth packages)))
+    (if (< base-depth (length packages))
+        (progn
+          (dotimes (dep (length packages))
+            (when (> dep base-depth)
+              (let ((target-packages (nth-value dep result)))
+                (mapcar (lambda (x) (delete x target-packages)) base-packages)
+                (setcar (nthcdr dep result) target-packages))))
+          (remove-duplicate-packages-in-depth result (1+ base-depth)))
+      packages)))
+
+(defun get-dependency-package-list ()
+  (let* ((package-list (get-package-list))
+         (dep-packages package-list)
+         (result (list package-list)))
+    (while dep-packages
+      (let ((deps
+             (cl-delete-duplicates
+              (mapcan
+               (lambda (pkg)
+                 (unless (package-installed-p pkg)
+                   (let* ((tr (package-compute-transaction () (list (list pkg))))
+                          (pkg-desc (car (last tr)))
+                          (deps (mapcar (lambda (x)
+                                          (if (package-installed-p (car x))
+                                              nil
+                                            (car x)))
+                                        (package-desc-reqs pkg-desc))))
+                     (remove nil deps))))
+               dep-packages))))
+        (setq dep-packages deps)
+        (when deps
+          (add-to-list 'result deps))))
+    result
+    ))
 
 (defun init-process-check (package-list proc-list output-buffer)
   (let ((any-live-proc t)
-        (log-left-packages package-list))
+        (log-left-packages (mapcar #'symbol-name package-list)))
     (while any-live-proc
       (setq any-live-proc nil)
       (dolist (proc proc-list)
@@ -59,39 +99,31 @@
                  (buffer-substring-no-properties
                   (point-min)
                   (point-max))))))
-        (dolist (pkg local-pkg-list)
-          (when (string-match
-                 (format "SK %s - Package is installed." pkg)
-                 content)
-            (let* ((pkg-len (length pkg))
-                   (padding (make-string (max 1 (- 25 pkg-len)) ?.)))
-              (message (format "%s %s..done." pkg padding)))
-            (setq log-left-packages (delete pkg log-left-packages)))))
+            (dolist (pkg local-pkg-list)
+              (when (string-match
+                     (format "SK %s - Package is installed." pkg)
+                     content)
+                (let* ((pkg-len (length pkg))
+                       (padding (make-string (max 1 (- 25 pkg-len)) ?.)))
+                  (message (format "%s %s..done." pkg padding)))
+                (setq log-left-packages (delete pkg log-left-packages)))))
       ;; (with-current-buffer output-buffer
       ;;   (message (buffer-substring 1 (point-max))))
       (sleep-for 2))))
 
-
-(defun init-function (&rest _)
-  (require 'cl)
-  (delete-directory "~/.emacs.d/elpa" t)
-  (package-refresh-contents)
-
-  (let* ((package-list (get-package-list))
-         (length-of-args (1+ (/ (length package-list) number-of-process)))
+(defun async-install-packages (package-list)
+  (let* ((length-of-args (1+ (/ (length package-list) number-of-process)))
          (left-packages package-list)
          (packages nil)
          (output-buffer (generate-new-buffer "install package"))
          (proc-list nil))
-    (message (format "\n%s packages will be installed.\n" (length package-list)))
-    ;; (print package-list)
 
     (while left-packages
       (setq packages
-            (cl-subseq left-packages
-                       0
-                       (min (length left-packages) length-of-args)))
-      ;; (message (format "len : %s, pkg : %s" length-of-args packages))
+            (mapcar #'symbol-name
+                    (cl-subseq left-packages
+                               0
+                               (min (length left-packages) length-of-args))))
       (setq left-packages (nthcdr length-of-args left-packages))
       (apply
        #'start-process
@@ -100,19 +132,29 @@
        "emacs" "-l" "~/.emacs.d/install.el" "-batch" "-install"
        packages)
       (add-to-list 'proc-list (get-buffer-process output-buffer)))
+    (init-process-check package-list proc-list output-buffer)))
 
-    (init-process-check package-list proc-list output-buffer)
+(defun init-function (&rest _)
+  (delete-directory "~/.emacs.d/elpa" t)
+  (package-refresh-contents)
+  (let* ((deps-list (get-dependency-package-list))
+         (packages-list (remove-duplicate-packages-in-depth deps-list))
+         (all-packages-count (length (apply #'append packages-list))))
+    ;; (print packages-list)
+    (message (format "\n%s packages will be installed.\n" all-packages-count))
+    (dolist (packages packages-list)
+      (async-install-packages packages))
+
     (message "Init done.")))
 
-
 (defun install-function (&rest r)
-  ;; (message (format "install : %s" command-line-args-left))
   (dolist (pkg command-line-args-left)
-    (let ((try-count 3))
-      (while (> try-count 0)
-        (setq try-count (- try-count 1))
+    (dotimes (try-count 3)
+      (when (not (require (intern pkg) nil t))
+        (print (format "try %s for %s" try-count pkg))
         (package-install (intern pkg) t)))
-    (message (format "SK %s - Package is installed." pkg))))
+    (when (require (intern pkg) nil t)
+      (message (format "SK %s - Package is installed." pkg)))))
 
 
 (add-to-list 'command-switch-alist '("-install" . install-function))
