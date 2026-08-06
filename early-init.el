efe5b13171ef5937b4aec81e71dea8c8aff7d37b
Seungki Kim <tttuuu888@gmail.com>
2026-08-07 08:08:28 +0900

    Add trailing slash to package-user-dir

--- early-init.el
+++ early-init.el
@@ -5,7 +5,7 @@
 ;; Redirect `user-emacs-directory'
 (defvar emacs-config-dir user-emacs-directory)
 (defvar emacs-etc-dir (expand-file-name "etc/" emacs-config-dir))
-(setq package-user-dir (expand-file-name "elpa" emacs-config-dir)
+(setq package-user-dir (expand-file-name "elpa/" emacs-config-dir)
       user-emacs-directory (expand-file-name ".local/" emacs-config-dir))
 
 ;; Redirect eln-cache folder
